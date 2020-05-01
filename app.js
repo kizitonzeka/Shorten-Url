a07f2a0e8a8ee7be0f512afcd0bbb52a63c0de59
Kizito Nzeka <kizzyche@ymail.com>
2020-05-01 15:03:11 +0100

    Add ejs layout

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get("/", function(req, res){
-    res.send("Server is working..");
+    res.render('home');
 })
 
 app.listen(3000, function(){
